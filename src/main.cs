16e0d804988282f103aa06b5f1d17ffd428b0f40
ItsHeet <137998620+ItsHeet@users.noreply.github.com>
2026-05-24 19:49:11 +0530

    codecrafters submit [skip ci]

--- src/main.cs
+++ src/main.cs
@@ -2,9 +2,9 @@ class Program
 {
     static void Main()
     {
+        Console.WriteLine("$ ");
         // TODO: Uncomment the code below to pass the first stage
         var Test=Console.ReadLine();
         Console.WriteLine($"{Test}:command not found");
-        Console.WriteLine("$ ");
     }
 }
